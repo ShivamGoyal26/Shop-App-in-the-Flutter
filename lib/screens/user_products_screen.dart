83deff7f65f06e149d9e507fd78d65b74735e1b2
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2020-03-25 17:10:16 +0530

    16th commit

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
 
 import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
+import '../screens/edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
+  static const routeName = 'user-products';
   @override
   Widget build(BuildContext context) {
     final productData = Provider.of<Products>(context);
@@ -13,15 +17,26 @@ class UserProductsScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(10),
         child: ListView.builder(
           itemCount: productData.items.length,
-          itemBuilder: (ctx, i) =>,
+          itemBuilder: (ctx, i) => Column(
+            children: <Widget>[
+              UserProductItem(
+                productData.items[i].title,
+                productData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/screens/orders_screen.dart';
 
+import '../screens/user_products_screen.dart';
+
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,6 +13,7 @@ class AppDrawer extends StatelessWidget {
             title: Text("Hello Friend!"),
             automaticallyImplyLeading: false,
           ),
+      
           Divider(),
           ListTile(
             leading: Icon(Icons.shop),
@@ -25,6 +28,14 @@ class AppDrawer extends StatelessWidget {
             title: Text("Orders"),
             onTap: (){
               Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName);
+            },
+          ),
+           Divider(),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text("Manage Products"),
+            onTap: (){
+              Navigator.of(context).pushReplacementNamed(UserProductsScreen.routeName);
             },
           ),
         ],
