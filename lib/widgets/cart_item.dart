22fb0a259021da0d815c049d9147d943b213fd98
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2020-03-25 12:03:58 +0530

    15th commit

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -35,6 +35,30 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+       return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text("Are You Sure?"),
+                  content:
+                      Text("Do you want to remove the item from the cart?"),
+                  actions: <Widget>[
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.of(ctx).pop(false);
+                      },
+                      child: Text("No"),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.of(ctx).pop(true);
+                      },
+                      child: Text("Yes"),
+                    )
+                  ],
+                ));
+        // return Future
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(
           context,
